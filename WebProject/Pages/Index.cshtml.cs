ad8b43fd524a11cdcda3ae9b86d250abea2a4bc1
M Reza Faisal <M Reza Faisal@WIN-M3AII0VJCEK>
2026-03-22 07:46:05 +0700

    Add project files.

---
+++ WebProject/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebProject.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
